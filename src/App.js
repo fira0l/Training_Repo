a313f32417af2e6d3fbd16c051544f381967b864
Firaol Anbessa <Firaolanbessa170@gmail.com>
2025-07-04 17:26:46 +0300

    Modifying and styling the cart button on the header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Fragment  } from 'react'
 import Header from './components/Layout/Header';
+import HeaderCartButton from './components/Layout/HeaderCartButton';
 
 
 
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,16 @@
 import {Fragment} from "react";
 
+import HeaderCartButton from '../../components/Layout/HeaderCartButton';
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+
+
 
 const Header = props => {
     return <Fragment>
         <header className={classes.header}>
             <h1>React Meals</h1>
-            <button>Cart</button>
+            <HeaderCartButton />
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt="A table full of delicious food!" />
