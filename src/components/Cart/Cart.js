6073ad767dca156874bccd40f63117835a1a7f14
Firaol Anbessa <Firaolanbessa170@gmail.com>
2025-07-04 21:31:47 +0300

    made the modal t be functional both ways

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,14 +11,14 @@ const Cart = (props) => {
   );
 
   return (
-    <Modal>
+    <Modal onClose={props.onClose}>
       {cartitems}
       <div className={classes.total}>
         <span className={classes.total}>Total Amount</span>
         <span className={classes.amount}>$35.62</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -10,7 +10,7 @@ const Header = props => {
     return <Fragment>
         <header className={classes.header}>
             <h1>React Meals</h1>
-            <HeaderCartButton />
+            <HeaderCartButton onClick={props.onShowCart}  />
         </header>
         <div className={classes['main-image']}>
             <img src={mealsImage} alt="A table full of delicious food!" />
