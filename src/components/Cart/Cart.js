315011b7c5d006b66b1fb08b434ca08214af2d3d
Firaol Anbessa <Firaolanbessa170@gmail.com>
2025-07-05 18:35:48 +0300

    Added the card item to the cart list to be shown automatically inside the cart after the user add them to the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,34 @@
+import { useContext } from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const addItemHandler = (item) => {}
+
+  const removeItemHandler = (id) => {
+    
+  };
+
   const cartitems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 22.99 }].map((item) => (
-        <li>{item.name} </li>
+      {cartCtx.items.map((item) => (
+        // <li key={item.id}>{item.name} </li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={removeItemHandler.bind(null, item.id)}
+          onAdd={addItemHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -15,14 +38,16 @@ const Cart = (props) => {
       {cartitems}
       <div className={classes.total}>
         <span className={classes.total}>Total Amount</span>
-        <span className={classes.amount}>$35.62</span>
+        <span className={classes.amount}>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>
-        <button className={classes.button}>Order</button>
+        <button className={classes["button--alt"]} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
-  );
+  ); 
 };
 
 export default Cart;
