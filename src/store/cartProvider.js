c3b756374d4eda7d0fa3167fc6c50644ac708364
Firaol Anbessa <Firaolanbessa170@gmail.com>
2025-07-05 17:57:15 +0300

    Creating the Add item to cart and remove item from the cart context for the cart items

--- src/store/cartProvider.js
+++ src/store/cartProvider.js
@@ -1,23 +1,65 @@
 
+
+import { useReducer } from "react";
 import CartContext from "./cart-context";
+
+ const defaultCartState = {
+        items: [],
+        totalAmount: 0
+    }
+
+ const cartReducer = (state, action) => {
+        if (action.type === 'ADD_ITEM') {
+            const updatedItems = state.items.concat(action.item);
+            const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+            return {
+                items: updatedItems,
+                totalAmount: updatedTotalAmount
+            }
+        } else if (action.type === 'REMOVE_ITEM') {
+            const updatedItems = state.items.filter(item => item.id !== action.id);
+            const updatedTotalAmount = state.totalAmount - state.items.find(item => item.id === action.id).price;
+            return {
+                items: updatedItems,
+                totalAmount: updatedTotalAmount
+            }
+        } else if (action.type === 'CLEAR_CART') {
+            return defaultCartState;
+        }
+
+        return defaultCartState
+    }
 
 const CartProvider = (props) => {
 
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+
     const addItemHandler = (item) => {
         // Logic to add item to cart
+        dispatchCartAction({
+            type: 'ADD_ITEM',
+            item: item
+        })
     };
 
     const removeItemHandler = (id) => {
         // Logic to remove item from cart 
+        dispatchCartAction({
+            type: 'REMOVE_ITEM',
+            id: id
+        });
     }
 
     const clearCartHandler = () => {
         // Logic to clear the cart
+        dispatchCartAction({
+            type: 'CLEAR_CART'
+        });
     }
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem: addItemHandler,
         removeItem: removeItemHandler,
         clearCart: clearCartHandler
